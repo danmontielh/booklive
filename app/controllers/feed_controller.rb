d78e5f87d7d98de644fbd78a221fe78d48db3e9e
Dan Montiel <danmontielh@gmail.com>
2019-06-18 18:39:52 -0500

    feed and omniauth ok

---
+++ app/controllers/feed_controller.rb
@@ -0,0 +1,6 @@
+class FeedController < ApplicationController
+    before_action :authenticate_user!, only: [:index]
+    def index
+        @user = User.find(current_user.id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => 'accounts', controllers: { registrations: "registrations", :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :feed
+  root 'home#index'
+
+  
+   
 end
