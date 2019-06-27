2b08b7fb5552cd32f3aa695a44489f2792cfad08
thebigsteph <nzali.stephane86@gmail.com>
2019-06-27 22:43:17 +0100

    ajax communication

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,6 @@
 class FriendshipsController < ApplicationController
 
-
+    before_action :set_friendship, only: [:show, :update, :destroy]
 
 
     def create
@@ -8,12 +8,21 @@ class FriendshipsController < ApplicationController
         if @friendship.save
             flash[:success] = "Invitation has sending !"
             respond_to do |format|
-                format.js { render 'users/create' }
+                format.js { render 'users/create', locals: { user: User.find(@friendship.invited_friend_id) }}
             end
         end
     end
+
+    def destroy
+        @friendship.destroy
+    end
+    
     
     private
+
+    def set_friendship
+        @friendship = Friendship.find(params[:id])
+    end
 
     def friendship_params
         params.require(:friendship).permit(:invited_friend_id)
