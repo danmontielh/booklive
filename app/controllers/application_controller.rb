f9b750a5577c8fdff38a21c282604f083a1e068c
thebigsteph <nzali.stephane86@gmail.com>
2019-07-05 09:38:31 +0100

    display post content, author, comments and likes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
 
-    def like?(post_id)
-        @like = current_user.likes.where(post_id: post_id).count
-        return true if @like > 0
-    end
     
+
 end
