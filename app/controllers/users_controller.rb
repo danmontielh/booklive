b2665818b3f45e15419bf37ecede44620fd03f1c
thebigsteph <nzali.stephane86@gmail.com>
2019-07-05 18:27:40 +0100

    set up a user show page with his post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @friendship = Friendship.new
-        
+        @feed = Post.order(created_at: :desc).where(user_id: params[:id])
+        @comment = Comment.new
+        @like = Like.new
     end
 end
