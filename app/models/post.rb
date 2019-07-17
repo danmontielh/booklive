599408de8077e7f6db2ea5fbbd8d550b062fa97d
Dan Montiel <danmontielh@gmail.com>
2019-07-17 17:41:28 -0500

    update ajax controller to likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,14 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
-  
-  
+
+
+  def self.timeline_friends(list_friends, user)
+    where("user_id IN (#{list_friends}) OR user_id = #{user.id}").order(created_at: :desc)
+  end
+
+  def self.timeline_only(user)
+    where("user_id = #{user.id}").order(created_at: :desc)
+  end
+
 end
