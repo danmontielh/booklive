b6bf94f771779398954a6bb19793ca02ea0cade3
Dan Montiel <danmontielh@gmail.com>
2019-07-19 12:34:41 -0500

    fix

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,7 +1,7 @@
 class FeedController < ApplicationController
     before_action :authenticate_user!, only: [:index]
     def index
-        @feed = current_user.get_posts
+        @feed_posts = current_user.get_posts
         @comment = Comment.new
         @like = Like.new
         @user = User.find(current_user.id)
